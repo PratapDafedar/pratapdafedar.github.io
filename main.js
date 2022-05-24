44c7258aead2fd091d94d2f7eb1a898d1042b7ea
Pratap Dafedar <pdafedar@gsngames.com>
2022-05-24 20:47:35 +0530

    Added basic demo for web rtc for connection check.

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
+// HTML elements
+const localVideo = document.getElementById('localVideo');
+const remoteVideo = document.getElementById('remoteVideo');
+const localInfoBox = document.getElementById('localInfo');
+const remoteInfoBox = document.getElementById('remoteInfo');
+
+const startButton = document.getElementById('start');
+const listenButton = document.getElementById('listen');
+const connectButton = document.getElementById('connect');
+
+const logLabel = document.getElementById('log');
+function log(message) {
+  logLabel.textContent = message;
+  console.log(message);
+}
 
 const servers = {
   iceServers: [
@@ -9,126 +24,50 @@ const servers = {
   iceCandidatePoolSize: 10,
 };
 
-// Global State
-const pc = new RTCPeerConnection(servers);
-let localStream = null;
-let remoteStream = null;
+const lc = new RTCPeerConnection(servers);
 
-let localData = null;
-let remoteData = null;
-
-// HTML elements
-const localVideo = document.getElementById('localVideo');
-const remoteVideo = document.getElementById('remoteVideo');
-const localInfoBox = document.getElementById('localInfo');
-const remoteInfoBox = document.getElementById('remoteInfo');
-const startButton = document.getElementById('start');
-const connectButton = document.getElementById('connect');
-const logLabel = document.getElementById('log');
-
-// 1. Setup media sources
 startButton.onclick = async () => {
-  localStream = await navigator.mediaDevices.getUserMedia({ video: false, audio: true });
-  remoteStream = new MediaStream();
-
-  localVideo.srcObject = localStream;
-  remoteVideo.srcObject = remoteStream;
-
-  // Push tracks from local stream to peer connection
-  localStream.getTracks().forEach((track) => {
-    pc.addTrack(track, localStream);
-  });
-
-  // Pull tracks from remote stream, add to video stream
-  pc.ontrack = (event) => {
-    event.streams[0].getTracks().forEach((track) => {
-      remoteStream.addTrack(track);
-    });
-  };
-
   
-  // Get candidates for caller, save to db
-  pc.onicecandidate = (event) => {
-    if(event.candidate != null)
-    {
-      localData = event.candidate.toJSON();
-      localInfoBox.value += JSON.stringify(localData);
-    }
-  };
-  
-  // Create offer
-  const offerDescription = await pc.createOffer();
-  await pc.setLocalDescription(offerDescription);
-  
-  const offer = {
-    sdp: offerDescription.sdp,
-    type: offerDescription.type,
-  };
+  const dc = lc.createDataChannel("channel");
+  dc.onmessage = e => console.log("received" + e.data);
+  dc.onopen = e => console.log("connection opened");
+  lc.onicecandidate = e => {
+    var newCandidate = JSON.stringify(lc.localDescription);
+    localInfoBox.value = newCandidate;
+  }
+  lc.createOffer()
+    .then(o => lc.setLocalDescription(o))
+    .then(a=> log("local desc created successfully!"));
 
-
-  connectButton.disabled = false;
 };
 
 
+listenButton.onclick = async () => {
   
-//   //await callDoc.set({ offer });
-//   await setDoc(callDoc, offer, { merge: true });
+  lc.onicecandidate = e => {
+    var newCandidate = JSON.stringify(lc.localDescription);
+    localInfoBox.value = newCandidate;
+  }
+  lc.ondatachannel = e => {
+    lc.dc = e.channel;
+    lc.dc.onopen = e => log("local desc created successfully!")
+    lc.dc.onmessage = e => {
+      console.log("new message from client: " + e.data);
+    }
+  }
+  
+  var offer = JSON.parse(remoteInfoBox.value);
+  lc.setRemoteDescription(offer).then(a => log("offer set!"))
+  
+  lc.createAnswer()
+  .then(a => lc.setLocalDescription(a))
+  .then(a => {
+    log("answer created!")
+  }); 
+}
 
-//   // Listen for remote answer
-//   callDoc.onSnapshot((snapshot) => {
-//     const data = snapshot.data();
-//     if (!pc.currentRemoteDescription && data?.answer) {
-//       const answerDescription = new RTCSessionDescription(data.answer);
-//       pc.setRemoteDescription(answerDescription);
-//     }
-//   });
 
-//   // When answered, add candidate to peer connection
-//   answerCandidates.onSnapshot((snapshot) => {
-//     snapshot.docChanges().forEach((change) => {
-//       if (change.type === 'added') {
-//         const candidate = new RTCIceCandidate(change.doc.data());
-//         pc.addIceCandidate(candidate);
-//       }
-//     });
-//   });
-
-//   hangupButton.disabled = false;
-// };
-
-// // 3. Answer the call with the unique ID
-// answerButton.onclick = async () => {
-//   const callId = callInput.value;
-//   const callDoc = firestore.collection('calls').doc(callId);
-//   const answerCandidates = callDoc.collection('answerCandidates');
-//   const offerCandidates = callDoc.collection('offerCandidates');
-
-//   pc.onicecandidate = (event) => {
-//     event.candidate && answerCandidates.add(event.candidate.toJSON());
-//   };
-
-//   const callData = (await callDoc.get()).data();
-
-//   const offerDescription = callData.offer;
-//   await pc.setRemoteDescription(new RTCSessionDescription(offerDescription));
-
-//   const answerDescription = await pc.createAnswer();
-//   await pc.setLocalDescription(answerDescription);
-
-//   const answer = {
-//     type: answerDescription.type,
-//     sdp: answerDescription.sdp,
-//   };
-
-//   await callDoc.update({ answer });
-
-//   offerCandidates.onSnapshot((snapshot) => {
-//     snapshot.docChanges().forEach((change) => {
-//       console.log(change);
-//       if (change.type === 'added') {
-//         let data = change.doc.data();
-//         pc.addIceCandidate(new RTCIceCandidate(data));
-//       }
-//     });
-//   });
-// };
+connectButton.onclick = async () => {
+  var answer = JSON.parse(remoteInfoBox.value);
+  lc.setRemoteDescription(answer);
+}
